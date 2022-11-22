dd360d6da21792f00a0162443176d29a343d2c8c
KimHyeonJin <hyeonjin9819@gmail.com>
2022-11-22 23:27:28 +0900

    feat/backend 초기설정

---
+++ fourdollar/src/test/java/com/zolho/project/fourdollar/FourdollarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zolho.project.fourdollar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourdollarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
