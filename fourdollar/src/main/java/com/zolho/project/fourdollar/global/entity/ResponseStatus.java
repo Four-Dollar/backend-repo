5570c86134c043983e46f06f85b5386e20dbb9ab
chynmn <chynmn0523@gmail.com>
2022-11-27 01:14:01 +0900

    [feat] ResponseStatus에 massage 추가함

--- fourdollar/src/main/java/com/zolho/project/fourdollar/global/entity/ResponseStatus.java
+++ fourdollar/src/main/java/com/zolho/project/fourdollar/global/entity/ResponseStatus.java
@@ -22,7 +22,13 @@ public enum ResponseStatus {
     // Head Name: F
     POST_FILE_SUCCESS("F000", "파일 업로드 성공"),
     DELETE_FILE_SUCCESS("F001", "파일 삭제 성공"),
-    GET_FILE_SUCCESS("F002", "파일 가져오기 성공");
+    GET_FILE_SUCCESS("F002", "파일 가져오기 성공"),
+
+    POST_USEDGOODS_SUCCESS("UG000", "상품 업로드 성공"),
+
+    DELETE_USEDGOODS_SUCCESS("UG001", "상품 삭제 성공"),
+
+    GET_USEDGOODS_SUCCESS("UG002", "상품 가져오기 성공");
 
     private final String code;
     private final String message;
